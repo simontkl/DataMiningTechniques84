b6c46f24919b66abaf9cf5537d4149ea1e1c4073
Kasper <kasper.lindhorst@student.uva.nl>
2021-04-06 14:36:57 +0200

    Git check function

---
+++ hello_there.py
@@ -0,0 +1,17 @@
+#Hello Emily and Simon. Please enter your name below and push to Git to see that the Git is working propperly
+
+def git_checker():
+
+    e='Emily'
+    s=''
+
+    if e=='Emily' and s=='':
+        print("Emily joined the mining company, but Simon didn't. Germans... *eye roll*")
+    elif e=='' and s=='Simon':
+        print("Simon joined the mining company, but Emily didn't. Tukkers... *eye roll*")
+    elif e=='Emily' and s=='Simon':
+        print("The mining party is assembled. LET'S GOOOOOOO")
+    else:
+        print("Hey Google, play 'Mad World' by Gary Jules, Imma sad")
+
+git_checker()
